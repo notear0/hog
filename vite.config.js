d15f71ed887a7b59922145ccc05a67917410d618
seung-heo <seung.heo@samsung.com>
2026-05-25 12:19:46 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // GitHub Pages 배포 시 저장소 이름으로 base 경로 설정
   // 예: https://yourname.github.io/swgoh-meta/ 이면 base: "/swgoh-meta/"
-  base: "/swgoh-meta/",
+  base: "/hog/",
 });
